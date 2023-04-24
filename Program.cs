05ebd08258d6be1d4bdf2fa2cd79e852f0d02762
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-24 20:05:44 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Receba um número. Exiba "par" ou "ímpar", conforme o caso.
+Console.WriteLine("--- Par ou Ímpar ---");
+
+Console.Write("Digite um número: ");
+int numero = int.Parse(Console.ReadLine()!);
+
+if (numero % 2 == 0) {
+    Console.WriteLine("par");
+} else {
+    Console.WriteLine("ímpar");
+}
